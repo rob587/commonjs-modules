288ba1a8001a5d7d2e84245ba97a27b96b444c92
rob587 <robertocamm02@yahoo.com>
2025-07-29 15:12:15 +0200

    function hobbies

---
+++ modules/hobbies.js
@@ -0,0 +1,8 @@
+// 2 - hobbies.js
+// Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
+// Esporta la funzione dal file.
+
+const hobbies = (hobbyOne, hobbyTwo, hobbyThree) =>{
+    return [hobbyOne, hobbyTwo, hobbyThree]
+}
+
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const names = require('./modules/names.js')
+const hobbies = require('./modules/hobbies.js')
 
 console.log(names)
+console.log(hobbies)
