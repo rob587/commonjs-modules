b489dc5107ba291d9063580c5842a7fd9c100246
rob587 <robertocamm02@yahoo.com>
2025-07-29 15:21:11 +0200

    [fix] file importati

---
+++ modules/people.js
@@ -0,0 +1,11 @@
+// 3 - people.js
+// Importa la tua funzione da names.js
+// Importa la tua funzione da hobbies.js
+// Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
+
+const names = require('./modules/names.js')
+const hobbies = require('./modules/hobbies.js')
+
+
+// console.log(names)
+// console.log(hobbies)
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const names = require('./modules/names.js')
 const hobbies = require('./modules/hobbies.js')
 
-console.log(names)
-console.log(hobbies)
+// console.log(names)
+// console.log(hobbies)
